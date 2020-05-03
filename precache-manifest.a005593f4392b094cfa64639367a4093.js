36e22b7c0aba68e62afb76cf6a8089e81d56995b
eletteric <60383026+eletteric@users.noreply.github.com>
2020-05-03 22:55:07 +0200

    first upload

---
+++ precache-manifest.a005593f4392b094cfa64639367a4093.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3997da4a073cd166e3b00e468e83b67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5908555e29a843c68a3",
+    "url": "/static/css/main.ec20dd05.chunk.css"
+  },
+  {
+    "revision": "438bfbaf50028f4b8b65",
+    "url": "/static/js/2.6d086c47.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.6d086c47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5908555e29a843c68a3",
+    "url": "/static/js/main.c854b673.chunk.js"
+  },
+  {
+    "revision": "4ca2f4b7b654e79a6037",
+    "url": "/static/js/runtime-main.ae692c5c.js"
+  },
+  {
+    "revision": "22e3f39f7fa056a3f34e2783c3240b82",
+    "url": "/static/media/logo-mihai.22e3f39f.svg"
+  },
+  {
+    "revision": "1854ab07ef55721756eee09fbf320037",
+    "url": "/static/media/mn.1854ab07.jpg"
+  }
+]);
